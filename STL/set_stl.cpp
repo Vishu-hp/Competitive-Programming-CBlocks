c729801a1967be73cf04e4cfe5c8bbb4164fd76d
DOM-Vishu <70435386+Vishu-hp@users.noreply.github.com>
2023-02-02 22:45:35 +0530

    Create set_stl.cpp

---
+++ STL/set_stl.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<string>
+#include<set>
+
+using namespace std;
+
+
+int main() {
+    
+    int arr[] = {10,20,11,9,8,11,10};
+    int n = sizeof(arr)/sizeof(int);
+
+    set<int> s;
+    for(int i=0;i<n;i++){
+        s.insert(arr[i]);
+    }
+
+
+    s.erase(10);
+    auto it  = s.find(11);
+    s.erase(it);
+
+    // Print all th elements
+    for(set<int>::iterator it=s.begin();it!=s.end();it++){
+        cout<<(*it)<<" ,";
+    }
+    cout<<endl;
+
+    return 0;
+}
